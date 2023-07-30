b5b256826b121ecad4bb8dddf9a188bd1c03bbe1
sundaravathanan14 <sundar.aero11@gmail.com>
2023-07-30 18:01:12 +0530

    structural directives

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from '../service/user.service';
 
 @Component({
@@ -7,8 +8,30 @@ import { UserService } from '../service/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(public userService:UserService){}
+  show =false;
+  items=[1,2,3,4,5,6,7,10];
+  students=[{
+    id:1,name:'a',class:10
+  },
+  {
+    id:2,name:'b',class:10
+  },
+  {
+    id:3,name:'c',class:10
+  },
+  {
+    id:4,name:'d',class:10
+  },
+  {
+    id:5,name:'e',class:10
+  }]
+  constructor(public userService:UserService,private router:Router){}
   changeName(data:string){
     this.userService.setName(data);
   }
+  navigate(){
+   console.log(this.router.url);
+    this.router.navigate(['/signup']);
+  }
+  //
 }
